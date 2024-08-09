5567bd573adb8244fc18f7ffce57afc851dde051
Sheyla Arellano <sheylarellanopsico@gmail.com>
2024-08-09 09:03:14 +0200

    log in a medio hacer

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function LoginPage() {
+
+  const {usuarioAutenticado} = useContext(AuthContext)
+  const navigate = useNavigate()
+
+  const [email, setEmail] = useState("");
+  const [contraseña, setContraseña] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlecontraseñaChange = (e) => setContraseña(e.target.value);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const credencialesUsuario ={
+      email,
+      contraseña
+    }
+
+try {
+
+await service.post("/auth/iniciar-sesion")
+
+
+  
+} catch (error) {
+  
+}
+
+
+
+
+
+  }
+
+
   return (
     <div>LoginPage</div>
   )
