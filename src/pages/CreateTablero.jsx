8f6ed55d6679550e3f1d8a5d3060c2967c87dd7b
Sheyla Arellano <sheylarellanopsico@gmail.com>
2024-08-15 15:52:16 +0200

    cambios en crear tu tablero y home page

--- src/pages/CreateTablero.jsx
+++ src/pages/CreateTablero.jsx
@@ -8,28 +8,28 @@ function CreateTablero() {
   const navigate = useNavigate();
   const [titulo, setTitulo] = useState("");
   const [descripcion, setDescripcion] = useState("");
-  const [imageUrl, setImageUrl] = useState(""); 
-  const [isUploading, setIsUploading] = useState(false); 
+  const [imageUrl, setImageUrl] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
   const [canvasImage, setCanvasImage] = useState("");
 
   const handleFileUpload = async (event) => {
     if (!event.target.files[0]) {
       return;
     }
-  
-    setIsUploading(true); 
-  
-    const uploadData = new FormData(); 
+
+    setIsUploading(true);
+
+    const uploadData = new FormData();
     uploadData.append("image", event.target.files[0]);
-  
+
     try {
       const response = await service.post("/upload", uploadData);
-  
+
       setImageUrl(response.data.imageUrl);
-      setIsUploading(false); 
+      setIsUploading(false);
     } catch (error) {
       console.error("Error uploading the image", error);
-      setIsUploading(false); 
+      setIsUploading(false);
     }
   };
 
@@ -38,7 +38,7 @@ function CreateTablero() {
     const nuevoTablero = {
       titulo,
       descripcion,
-      imagen: canvasImage || imageUrl
+      imagen: canvasImage || imageUrl,
     };
 
     try {
@@ -51,57 +51,72 @@ function CreateTablero() {
 
   return (
     <div>
-      <h1>Tablero de expresión creativa</h1>
+      <div className="tablero-card-titulo">
+        <h1>Tablero de expresión creativa</h1>
+      </div>
       <br />
-      <p>
-        En esta sección, nuestros usuarios pueden subir dibujos, fotografías u
-        otras formas de arte que les permitan expresar sus emociones y narrar
-        sus historias de resistencia. Este espacio busca promover una visión
-        diversa y justa de las experiencias migrantes y racializadas, celebrando
-        la riqueza cultural y la resiliencia a través de la creatividad.
-      </p>
-      <Card border="dark" style={{ width: "100%"}}>
-        <h3 className="my-4">Crea tu Tablero</h3>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="d-flex flex-column justify-content-center align-items-center mb-5" controlId="formGridTitulo">
-            <Form.Label>Título: </Form.Label>
-            <Form.Control
-              type="text"
-              className="custom-form-control"
-              value={titulo}
-              onChange={(e) => setTitulo(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group className="d-flex flex-column justify-content-center align-items-center mb-5" controlId="formGridDescripcion">
-            <Form.Label>Descripción: </Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={6}
-              placeholder="Introduce una breve explicación del tablero"
-              className="custom-form-control"
-              value={descripcion}
-              onChange={(e) => setDescripcion(e.target.value)}
-            />
-          </Form.Group>
+      <div className="tablero-box">
+        <Card
+        className="signup-card"
+          border="dark"
+          style={{ width: "60%", height: "60%", marginLeft: "3%" }}
+        >
+          <h3 className="my-4">Crea tu Tablero</h3>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group
+              className="d-flex flex-column justify-content-center align-items-center mb-5"
+              controlId="formGridTitulo"
+            >
+              <Form.Label>Título: </Form.Label>
+              <Form.Control
+                type="text"
+                className="custom-form-control"
+                value={titulo}
+                onChange={(e) => setTitulo(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              className="d-flex flex-column justify-content-center align-items-center mb-5"
+              controlId="formGridDescripcion"
+            >
+              <Form.Label>Descripción: </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={6}
+                placeholder="Introduce una breve explicación del tablero"
+                className="custom-form-control"
+                value={descripcion}
+                onChange={(e) => setDescripcion(e.target.value)}
+              />
+            </Form.Group>
 
-          <Form.Label>Dibujo en canvas: </Form.Label>
-          <Canvas />
+            <Form.Label>Dibujo en canvas: </Form.Label>
+            <Canvas />
 
-          <Form.Group className="d-flex flex-column justify-content-center align-items-center mb-5" controlId="formGridImagen">
-            <Form.Label>Imagen: </Form.Label>
-            <Form.Control
-              type="file"
-              className="custom-form-control"
-              onChange={handleFileUpload}
-              disabled={isUploading}
-            />
-          </Form.Group>
-          
-          <Button variant="dark" type="submit" className="mb-5" disabled={isUploading || !imageUrl} >
-            Crear
-          </Button>
-        </Form>
-      </Card>
+            <Form.Group
+              className="d-flex flex-column justify-content-center align-items-center mb-5"
+              controlId="formGridImagen"
+            >
+              <Form.Label>Imagen: </Form.Label>
+              <Form.Control
+                type="file"
+                className="custom-form-control"
+                onChange={handleFileUpload}
+                disabled={isUploading}
+              />
+            </Form.Group>
+
+            <Button
+              variant="dark"
+              type="submit"
+              className="mb-5"
+              disabled={isUploading || !imageUrl}
+            >
+              Crear
+            </Button>
+          </Form>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from "react";
 
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../assets/MentEmigrante-logo.png";
-import cardImageOne from "../assets/vector_1.png"
+import cardImageOne from "../assets/vector_1.png";
+import cardImageTwo  from "../assets/vector_2.png"
 import { AuthContext } from "../context/auth.context";
+import imageCaruseltaller1 from "../assets/taller1.jpg";
+import imageCaruseltaller2 from "../assets/taller2.jpg";
+import imageCaruseltaller3 from "../assets/taller4.png";
+import imageCarusel1 from "../assets/tablero1.jpeg";
+import imageCarusel2 from "../assets/tablero2.jpeg";
+import imageCarusel3 from "../assets/tablero3.jpg";
+
 
 function HomePage() {
-
-  const {estaLoggeado} = useContext(AuthContext)
+  const { estaLoggeado } = useContext(AuthContext);
 
   return (
     <div>
@@ -19,34 +26,51 @@ function HomePage() {
           <h1>Tu espacio seguro en cualquier lugar</h1>
           <h4>
             App de salud mental para personas migras de Latinoamérica que buscan
-            recibir un apoyo. Hay talleres todos los días: regístrate para unirte
-            a la diversión.
+            recibir un apoyo. Hay talleres todos los días: regístrate para
+            unirte a la diversión.
           </h4>
-          {!estaLoggeado && <Link to={"/registro"}>
-            <Button className="botonRegistro mb-5" type="submit">
-              Unirse a Mentemigrante
-            </Button>
-          </Link>}
-          
+          {!estaLoggeado && (
+            <Link to={"/registro"}>
+              <Button className="botonRegistro mb-5" type="submit">
+                Unirse a Mentemigrante
+              </Button>
+            </Link>
+          )}
         </div>
       </div>
       <div className="cards-home">
         <h3>Cómo funciona Mentemigrante</h3>
         <br />
         <div className="cards-home-content">
-          <Card style={{backgroundColor: '#173B45', color: '#F8EDED', padding: '5% 0'}}>
+          <Card
+            style={{
+              backgroundColor: "#173B45",
+              color: "#F8EDED",
+              padding: "5% 0",
+            }}
+          >
             <Card.Body>
-              <Card.Title> <b>Entre migras nos sostenemos</b></Card.Title>
-              <Card.Img src={cardImageOne} style={{width: "50%"}}></Card.Img>
+              <Card.Title>
+                {" "}
+                <b>Entre migras nos sostenemos</b>
+              </Card.Title>
+              <Card.Img src={cardImageOne} style={{ width: "50%" }}></Card.Img>
               <Card.Text>
                 Aquí encontrarás un espacio seguro para expresar tu malestar y
                 recibir apoyo desde una perspectiva decolonial y antirracista.
               </Card.Text>
             </Card.Body>
           </Card>
-          <Card style={{backgroundColor: '#173B45', color: '#F8EDED', padding: '5% 0' }}>
+          <Card
+            style={{
+              backgroundColor: "#173B45",
+              color: "#F8EDED",
+              padding: "5% 0",
+            }}
+          >
             <Card.Body>
               <Card.Title>¿Qué ofrecemos?</Card.Title>
+              <Card.Img src={cardImageTwo} style={{ width: "25%"}}></Card.Img>
               <Card.Text>
                 Te ayudamos a cuestionar y desafiar las estructuras de poder que
                 afectan tu vida, para que puedas cuidar tu salud mental mientras
@@ -54,6 +78,84 @@ function HomePage() {
               </Card.Text>
             </Card.Body>
           </Card>
+        </div>
+      </div>
+      <div className="carousel-div">
+        <div>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCaruseltaller1}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Talleres</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCaruseltaller2}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Talleres</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCaruseltaller3}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Talleres</h3>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCarusel1}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Tablero Creativo</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCarusel2}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Tablero Creativo</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src={imageCarusel3}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>Tablero Creativo</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </div>
       </div>
     </div>
